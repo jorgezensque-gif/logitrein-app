f19ab72ffd6e9999de0e38160237001299e1191b
Jorge Zensque- projetos <jorgezensque@gmail.com>
2026-04-05 12:29:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 st.set_page_config(page_title='LogiTrein 4.0', layout='wide')
 
-# O Streamlit vai ler o arquivo logitrein.html que você vai subir
-with open('logitrein.html', 'r', encoding='utf-8') as f:
-    html_data = f.read()
+# Pega o caminho absoluto da pasta onde o script está rodando
+current_dir = os.path.dirname(os.path.abspath(__file__))
+html_path = os.path.join(current_dir, 'logitrein.html')
 
-components.html(html_data, height=1200, scrolling=True)
+if os.path.exists(html_path):
+    with open(html_path, 'r', encoding='utf-8') as f:
+        html_data = f.read()
+    
+    # Ajuste o height para 100vh (altura total da tela)
+    components.html(html_data, height=1000, scrolling=True)
+else:
+    st.error(f"Arquivo 'logitrein.html' não encontrado. Verifique se o nome no GitHub está correto.")
